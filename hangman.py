660d30eab18c217a4c7f72d6e55a6fba67d20ad0
ja13n <1jalen.e13@gmail.com>
2022-10-06 06:34:32 -0400

    hangman game

---
+++ hangman.py
@@ -0,0 +1,84 @@
+import random
+from collections import Counter
+class Hangman:
+ 
+    def __init__(self):
+        self.someWords = ["apple", "banana", "mango", "strawberry",
+        "orange", "grape", "pineapple", "apricot", "lemon", "coconut", "watermelon"
+        "cherry", "papaya", "berry", "peach", "lychee", "muskmelon"]
+
+        self.word = random.choice(self.someWords)  
+
+    def game(self):
+        if __name__ == '__main__':
+            print('Guess the word! HINT: word is a name of a fruit')      
+        for i in range(len(self.word)):
+            # For printing the empty spaces for letters of the word
+            print('_', end=' ')       
+ 
+        playing = True
+        # list for storing the letters guessed by the player
+        letterGuessed = ''               
+        chances = len(self.word) + 2
+        correct = 0
+        flag = 0
+        try:
+            while (chances != 0) and flag == 0: #flag is updated when the word is correctly guessed
+                print()
+                chances -= 1
+    
+                try:
+                    guess = str(input('Enter a letter to guess: '))
+                except:
+                    print('Enter only a letter!')
+                    continue
+    
+                # Validation of the guess
+                if not guess.isalpha():
+                    print('Enter only a LETTER')
+                    continue
+                elif len(guess) > 1:
+                    print('Enter only a SINGLE letter')
+                    continue
+                elif guess in letterGuessed:
+                    print('You have already guessed that letter')
+                    continue
+    
+    
+                # If letter is guessed correctly
+                if guess in self.word:
+                    k = self.word.count(guess) #k stores the number of times the guessed letter occurs in the word
+                    for _ in range(k):   
+                        letterGuessed += guess # The guess letter is added as many times as it occurs
+    
+                # Print the word
+                for char in self.word:
+                    if char in letterGuessed and (Counter(letterGuessed) != Counter(self.word)):
+                        print(char, end = ' ')
+                        correct += 1
+                    # If user has guessed all the letters
+                    elif (Counter(letterGuessed) == Counter(self.word)): # Once the correct word is guessed fully,
+                                                                    # the game ends, even if chances remain
+                        print("The word is: ", end=' ')
+                        print(self.word)
+                        flag = 1
+                        print('Congratulations, You won!')
+                        break # To break out of the for loop
+                        break # To break out of the while loop
+                    else:
+                        print('_', end = ' ')
+                        
+    
+            # If user has used all of his chances
+            if chances <= 0 and (Counter(letterGuessed) != Counter(self.word)):
+                print()
+                print('You lost! Try again..')
+                print('The word was {}'.format(self.word))
+    
+        except KeyboardInterrupt:
+            print()
+            print('Bye! Try again.')
+            exit()
+
+player = Hangman()
+player.game()
